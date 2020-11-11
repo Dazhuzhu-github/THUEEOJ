cefeb8993f1d78d6fc0894a7e3c900a36f20694d
dazhuzhu <1787938852@qq.com>
2020-11-11 22:00:01 +0800

    试用korofileHeader

--- oj/07/07.cpp
+++ oj/07/07.cpp
@@ -1,3 +1,11 @@
+/*
+ * @Author: your name
+ * @Date: 2020-11-10 16:29:34
+ * @LastEditTime: 2020-11-11 21:58:34
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \07\07.cpp
+ */
 #include<stdio.h>
 int main()
 {
